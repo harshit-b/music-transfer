bddb6e3e8bcc8f3a4e8ebe0d460d03cda323bf95
Harshit Bhargava <harshit.bhargava@ttu.edu>
2023-10-09 15:31:24 -0500

    started documentation - spotify backend

--- server/controllers/spotifyController.js
+++ server/controllers/spotifyController.js
@@ -5,6 +5,9 @@ const client_id = "06d6e4c4bfd34e0ba2d776f2f18ffb09"
 const redirect_uri = "http://localhost:4000/spotify/callback"
 const client_secret = "b513d75dc33f4f78be7dbbfa852b13dc"
 
+//Initiate OAuth flow - Spotify
+//Input in the query - userId: ID of the user in mongoDB database
+//userID used as the state 
 module.exports.spotifyAuth = async (req, res) => {
     const state = req.query.userId;
     console.log("While logging in" + state)
@@ -20,6 +23,8 @@ module.exports.spotifyAuth = async (req, res) => {
       }));
 }
 
+//Callback function getting response from spotify login API
+//Response: code, state(userID) if accepted 
 module.exports.spotifyCallback = async (req, res) => {
   try {
     var code = req.query.code || null;
@@ -43,6 +48,7 @@ module.exports.spotifyCallback = async (req, res) => {
       const tokenExpiresIn = Math.floor(Date.now() / 1000) + tokenResponse.data.expires_in; 
       const filter = {_id : userId}
       const update = {spotifyAccessToken : accessToken, spotifyRefreshToken : refreshToken, spotifyTokenExpiresIn : tokenExpiresIn }
+      //Store Access Token and Refresh Token of user in database
       await User.findOneAndUpdate(filter, update);
       res.redirect(`http://localhost:3000/`)
       // res.redirect(`http://localhost:3000/spotify/userProfile/${userId}`)
@@ -56,6 +62,9 @@ module.exports.spotifyCallback = async (req, res) => {
   }
 }
 
+//Function to fetch playlist info of the user
+//Input in query - userID
+//userID to fetch accessToken
 module.exports.spotifyUserPlaylists = async(req,res) => {
   try {
     const userId = req.query.userId;
@@ -64,7 +73,9 @@ module.exports.spotifyUserPlaylists = async(req,res) => {
     
     if (user) {
       accessToken = user.spotifyAccessToken;
+      //If access token expired, use refresh token to get new access token and replace 
       if (isAccessTokenExpired(user.spotifyTokenExpiresIn)) {
+        //Call function to call api to fetch new access token by inputting refresh token
         const {accessToken, expiresIn} = refreshToken(user.spotifyRefreshToken)
         await User.findOneAndUpdate({_id: userId}, {spotifyAccessToken : accessToken, spotifyTokenExpiresIn : expiresIn});
       } 
@@ -73,7 +84,7 @@ module.exports.spotifyUserPlaylists = async(req,res) => {
       res.status(500).json({ error: 'Failed to fetch access token' });
     }
 
-    // Make an API request to fetch the user's profile information
+    // Make an API request to fetch the user's playlists information
     const userPlaylistsResponse = await axios.get('https://api.spotify.com/v1/me/playlists', {
       headers: {
         'Authorization': `Bearer ${accessToken}`,
@@ -81,10 +92,10 @@ module.exports.spotifyUserPlaylists = async(req,res) => {
     });
 
     if (userPlaylistsResponse.status === 200) {
-      // Extract user profile information
+      // Extract user playlists information
       const userPlaylists = userPlaylistsResponse.data;
 
-      // Redirect the user to the frontend with user info in query parameters
+      // Redirect the user to the frontend with user playlist info in query parameters
       res.json({userPlaylists});
     } else {
       res.status(userPlaylistsResponse.status).json({ error: 'Failed to fetch user profile' });
@@ -95,6 +106,9 @@ module.exports.spotifyUserPlaylists = async(req,res) => {
   }
 }
 
+//Function to fetch user info of the user
+//A lot of same code as playlist function
+// TODO: Refactor Code
 module.exports.spotifyUserProfile = async (req, res) => {
   try {
     const userId = req.query.userId;
@@ -134,6 +148,9 @@ module.exports.spotifyUserProfile = async (req, res) => {
   }
 }
 
+//function to check if access token has expired or not
+//Logic: while authenticating user also store the time when access token will expire, 
+//        if current time exceeds time stored in database, then token expired
 function isAccessTokenExpired(spotifyTokenExpiresIn) {
   let result;
   Math.floor(Date.now() / 1000) < spotifyTokenExpiresIn ? result = false : result = true;
@@ -143,8 +160,6 @@ function isAccessTokenExpired(spotifyTokenExpiresIn) {
 // Helper function to refresh the access token using the refresh token
 async function refreshToken(refreshToken) {
   try {
-    // Implement the logic to refresh the access token using the provided refresh token
-    // Make a request to Spotify's token endpoint with the refresh token
     const tokenResponse = await axios.post('https://accounts.spotify.com/api/token', null, {
       params: {
         grant_type: 'refresh_token',
