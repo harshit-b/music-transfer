d7b0429e2676db2fb6657bc459642647198db8d7
Harshit Bhargava <harshit.bhargava@ttu.edu>
2023-11-17 13:45:57 -0600

    added frontend url to backend cors to allow req

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/authRoute");
-const { MONGO_URL, PORT, FRONTEND_URL } = process.env;
+const { MONGO_URL, PORT, FRONTEND_URL, FRONTEND_URL_2 } = process.env;
 
 mongoose
   .connect(MONGO_URL, {
@@ -18,7 +18,7 @@ mongoose
 
 app.use(
   cors({
-    origin: [FRONTEND_URL],
+    origin: [FRONTEND_URL, FRONTEND_URL_2],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
